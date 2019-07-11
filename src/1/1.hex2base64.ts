0e06be6c5cb1debfd1acd14257d40ffc51ff6dc5
winderica <winderica@gmail.com>
2019-07-11 15:42:10 +0800

    Update: minor improvements

--- src/1/1.hex2base64.ts
+++ src/1/1.hex2base64.ts
@@ -1,7 +1,10 @@
 export function base64Encode(str: string) {
+    return base64EncodeFromCharCode(new TextEncoder().encode(str));
+}
+
+export function base64EncodeFromCharCode(arr: Uint8Array) {
     const table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-    return [...new TextEncoder().encode(str)]
-        .map((i) => i.toString(2).padStart(8, '0'))
+    return [...arr].map((i) => i.toString(2).padStart(8, '0'))
         .join('')
         .match(/.{1,6}/g)!
         .map((i) => table[parseInt(i.padEnd(6, '0'), 2)] + '='.repeat((6 - i.length) / 2 % 3))
@@ -11,15 +14,16 @@ export function base64Encode(str: string) {
 export function base64DecodeAsCharCode(str: string) {
     const table = Object.fromEntries([...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'].map((i, j) => [i, j]));
     // TODO: validation
-    return [...str]
+    return Uint8Array.from([...str]
         .map((i) => i === '=' ? '' : table[i].toString(2).padStart(6, '0'))
         .join('')
         .match(/.{8}/g)!
-        .map((i) => parseInt(i, 2));
+        .map((i) => parseInt(i, 2))
+    );
 }
 
 export function base64Decode(str: string) {
-    return new TextDecoder().decode(Uint8Array.from(base64DecodeAsCharCode(str)));
+    return new TextDecoder().decode(base64DecodeAsCharCode(str));
 }
 
 export function hex2base64(str: string) {
