8b1caeb937f588b2889fe1beb404b9633c1c2555
winderica <winderica@gmail.com>
2019-07-16 11:19:02 +0800

    Add: add set 3 challenge 19

---
+++ test/3/19.test.ts
@@ -0,0 +1,8 @@
+import { fixedNonce } from 'set3/19.CTRFixedNonce';
+
+describe('Set 3 Challenge 19', () => {
+    it('should produce right answer', (done) => {
+        fixedNonce();
+        done();
+    });
+});
