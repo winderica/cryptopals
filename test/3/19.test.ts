aebab0e1aeaa23fc8e2fee2da559c96e0d7a5c82
winderica <winderica@gmail.com>
2019-07-16 11:42:08 +0800

    Update: minor improvements

--- test/3/19.test.ts
+++ test/3/19.test.ts
@@ -1,8 +1,11 @@
+import { readFileSync } from 'fs';
+import { base64Decode } from 'set1/1.hex2base64';
 import { fixedNonce } from 'set3/19.CTRFixedNonce';
 
 describe('Set 3 Challenge 19', () => {
     it('should produce right answer', (done) => {
-        console.log(fixedNonce());
+        const strings = readFileSync('src/assets/19.txt').toString().split('\n').filter((i) => i).map(base64Decode);
+        console.log(fixedNonce(strings));
         done();
     });
 });
