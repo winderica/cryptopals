f4b1df7b1c351fe9f25ed5897335477201f39f5a
winderica <winderica@gmail.com>
2019-07-22 18:53:31 +0800

    Update: minor improvements

--- src/utils/sha1.ts
+++ src/utils/sha1.ts
@@ -4,8 +4,22 @@ import { rotateLeft } from 'utils/rotate';
 import { concatUint8 } from 'utils/uint';
 
 export class SHA1 {
-    private readonly h = Uint32Array.from([0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]);
+    protected readonly h = Uint32Array.from([0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]);
     private readonly k = [0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6];
+
+    pad(message: string) {
+        const length = message.length;
+        const padded = new Uint8Array(~~((length - 56) / 64 + 2) * 64);
+        str2bytes(message).forEach((i, j) => padded[j] = i);
+        padded[length] = 0x80;
+        let quotient = length * 8;
+        let index = padded.length - 1;
+        for (; quotient >= 256; quotient = ~~(quotient / 256), index--) {
+            padded[index] = quotient % 256;
+        }
+        padded[index] = quotient;
+        return padded;
+    }
 
     digest(message: string) {
         chunkArray(this.pad(message), 64).map((chunk) => {
@@ -22,19 +36,5 @@ export class SHA1 {
             [a, b, c, d, e].map((i, j) => this.h[j] += i);
         });
         return [...this.h].map((i) => i.toString(16).padStart(8, '0')).join('');
-    }
-
-    private pad(message: string) {
-        const length = message.length;
-        const padded = new Uint8Array(~~((length - 56) / 64 + 2) * 64);
-        str2bytes(message).forEach((i, j) => padded[j] = i);
-        padded[length] = 0x80;
-        let quotient = length * 8;
-        let index = padded.length - 1;
-        for (; quotient >= 256; quotient = ~~(quotient / 256), index--) {
-            padded[index] = quotient % 256;
-        }
-        padded[index] = quotient;
-        return padded;
     }
 }
--- test/utils/sha1.test.ts
+++ test/utils/sha1.test.ts
@@ -5,7 +5,7 @@ import { SHA1 } from 'utils/sha1';
 describe('Simple SHA1', () => {
     it('should produce right answer', (done) => {
         const message = randomString(100000);
-        expect(new SHA1().digest(message)).toBe(createHash('sha1').update(message).digest('hex'));
+        expect(new SHA1().digest(message)).toBe(createHash('sha1').update(message, 'ascii').digest('hex'));
         done();
     });
 });
