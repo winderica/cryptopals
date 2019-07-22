d8805ae7caf7ab8a654bb131441c6c529232a7e2
winderica <winderica@gmail.com>
2019-07-22 18:54:46 +0800

    Update: minor improvements

--- test/4/29.test.ts
+++ test/4/29.test.ts
@@ -1,9 +1,10 @@
 import { lengthExtension } from 'set4/29.SHA1LengthExtension';
+import { randomString } from 'utils/random';
 import { SHA1 } from 'utils/sha1';
 
 describe('Set 4 Challenge 29', () => {
     it('should produce right answer', (done) => {
-        const key = '1'.repeat(16);
+        const key = randomString();
         const newMessage = ';admin=true';
         const { message, hash } = lengthExtension(key, newMessage);
         expect(hash).toBe(new SHA1().digest(key + message));
